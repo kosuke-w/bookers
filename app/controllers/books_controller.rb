3bff43f3b032add489de1699b110bbbfafee33de
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-06-19 12:58:44 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,18 @@
 class BooksController < ApplicationController
-  def top
+
+  def new
+    @book = Book.new
   end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/"
+  end 
   
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+
 end
