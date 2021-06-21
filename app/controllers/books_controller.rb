b5319b5fcc9f92dd9e9ffed88c4af572e8a015b7
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-06-21 13:53:30 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
     book.save
@@ -19,13 +15,9 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def confirm
-    @book = Book.new(book_params)
-  end
-
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
