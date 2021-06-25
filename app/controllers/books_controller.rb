dbdfe00210d742d7b236393b931a2fd303164861
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-06-25 12:31:46 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    @books = Book.new(book_params)
+    if @books.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
   end
 
   def index
@@ -20,16 +23,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book)
+    @books = Book.find(params[:id])
+    if @books.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to books_path
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
-  end 
+  end
 
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "top" => "homes#top"
+  root :to => "homes#top"
   get "books" => "books#index"
   post "books" => "books#create"
   get "books/:id" => "books#show", as: "book"
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  
+
   resources :books
 
 end
